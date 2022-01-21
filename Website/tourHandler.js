2150d6b5ced14af5010303d7ab20c6a648dd13bf
Lua <jussyman13@gmail.com>
2022-01-21 01:01:36 +0100

    added prototype of tour, small revisions
    TODO:
    
    JSON field with titles and description of each image.

---
+++ Website/tourHandler.js
@@ -0,0 +1,56 @@
+const tourImg = document.getElementById('tourImage');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+const imgLbl = document.getElementById('imgLbl');
+
+nextImg = 2;
+currentImg = nextImg - 1;
+prevImg = 0;
+
+function next(){
+    if (nextImg == 31){
+        console.log("Called image does not exist");
+    }
+    else if (tourImg.src != "img" + currentImg + ".png"){
+        tourImg.src = tourImg.src = "img/" + nextImg + ".png";
+        nextImg = nextImg + 1;
+        currentImg = nextImg - 1;
+        prevImg = prevImg + 1;
+        imgLbl.textContent = currentImg + "/30";
+    }
+    else {
+        tourImg.src = "img/1.png";
+        nextImg = 2;
+        currentImg = nextImg - 1;
+        prevImg = 0;
+        imgLbl.textContent = currentImg + "/30";
+    }
+}
+
+function prev(){
+    if (prevImg == 0){
+        console.log("Called image does not exist");
+    }
+
+    else if (tourImg.src != "img" + currentImg + ".png"){
+        tourImg.src = tourImg.src = "img/" + prevImg + ".png";
+        nextImg = nextImg - 1;
+        currentImg = nextImg - 1;
+        prevImg = prevImg - 1;
+        imgLbl.textContent = currentImg + "/30";
+    }
+    else if (tourImg.src != "img/" + 0 && 30 + ".png"){
+        tourImg.src = "img/1.png";
+        nextImg = 2;
+        currentImg = nextImg - 1;
+        prevImg = 0;
+        imgLbl.textContent = currentImg + "/30";
+    }
+    else{
+        tourImg.src = "img/1.png";
+        nextImg = 2;
+        currentImg = nextImg - 1;
+        prevImg = 0;
+        imgLbl.textContent = currentImg + "/30";
+    }
+}
