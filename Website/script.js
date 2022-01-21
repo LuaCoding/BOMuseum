e3ca4467abbab876e13c6b7fe8690aeace7461c0
SHKlarenbeek <34334@ma-web.nl>
2022-01-21 10:44:11 +0100

    test

--- Website/script.js
+++ Website/script.js
@@ -1,6 +1,7 @@
 const slideshow = document.getElementById('slideshow');
 const slide = document.getElementById('slide');
 
+
 let sliders = [
     {
         "image":"img/amsterdamse-haven_0.jpg"
@@ -22,3 +23,19 @@ let sliders = [
 function show(index){
     slide.src = sliders[index].image;
 }
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("header");
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  } 
+
+  show(0);
