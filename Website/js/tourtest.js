5efff4a3dd3551d6bd4cbdb7d4d514accb1e4482
SHKlarenbeek <34334@ma-web.nl>
2022-02-01 11:34:32 +0100

    Tour test

--- Website/js/tourtest.js
+++ Website/js/tourtest.js
@@ -14,40 +14,48 @@ let directionButtons = {
     "zuid": document.getElementById('knopZuid')
 }
 
-let current_index = 1;
+let current_index = 0;
 
 let locatie =[
     {
         
         "img":"img/1.png",
-        "directions": {"noord": 2 }
+        "directions": {"noord": 1 }
     },
     {
         
         "img":"img/2.png",
         "directions": {
-            "zuid": 1,
-            "noord": 3
+            "zuid": 0,
+            "noord": 2
     }
     },
     {
         
         "img":"img/3.png",
         "directions": {
-            "noord": 4,
-            "zuid": 2}
+            "noord": 3,
+            "zuid": 1}
     },
     {
         
         "img":"img/4.png",
         "directions": {
-            "noord": 5,
-            "zuid": 3
+            "noord": 4,
+            "zuid": 2
         }
     },
     {
         
         "img":"img/5.png",
+        "directions": {
+            "west": 5,
+            "zuid": 3
+        }
+    },
+    {
+        
+        "img":"img/6.png",
         "directions": {
             "west": 6,
             "zuid": 4
@@ -55,105 +63,105 @@ let locatie =[
     },
     {
         
-        "img":"img/6.png",
-        "directions": {
-            "west": 7,
-            "zuid": 5
-        }
-    },
-    {
-        
         "img":"img/7.png",
         "directions": {
-            "noord": 8,
-            "zuid": 6
+            "noord": 7,
+            "zuid": 5
         }
     },
     {
         
         "img":"img/8.png",
         "directions": {
-            "oost": 9,
-            "zuid": 7,
-            "noord": 11
+            "oost": 8,
+            "zuid": 6,
+            "noord": 10
         }
     },
     {
         
         "img":"img/9.png",
         "directions": {
-            "noord": 10,
-            "west": 8
+            "noord": 9,
+            "west": 7
         }
     },
     {
         
         "img":"img/10.png",
         "directions": {
-            "noord": 11,
-            "zuid": 9
+            "noord": 10,
+            "zuid": 8
         }
     },
     {
         
         "img":"img/11.png",
         "directions": {
-            "oost": 8,
-            "zuid": 12,
-            "noord": 10
+            "oost": 7,
+            "zuid": 11,
+            "noord": 9
         }
     },
     {
         
         "img":"img/12.png",
         "directions": {
-            "noord": 13,
-            "zuid": 11
+            "noord": 12,
+            "zuid": 10
         }
     },
     {
         
         "img":"img/13.png",
         "directions": {
-            "noord": 14,
-            "west": 21,
-            "zuid": 12
+            "noord": 13,
+            "west": 20,
+            "zuid": 11
         }
     },
     {
         
         "img":"img/14.png",
         "directions": {
-            "zuid": 13,
-            "noord": 15
+            "zuid": 12,
+            "noord": 14
     }
     },
     {
         
         "img":"img/15.png",
         "directions": {
-            "noord": 14,
-            "zuid": 16}
+            "noord": 13,
+            "zuid": 15}
     },
     {
         
         "img":"img/16.png",
         "directions": {
-            "noord": 17,
-            "zuid": 15
+            "noord": 16,
+            "zuid": 14
         }
     },
     {
         
         "img":"img/17.png",
         "directions": {
-            "west": 18,
-            "zuid": 16
+            "west": 17,
+            "zuid": 15
         }
     },
     {
         
         "img":"img/18.png",
+        "directions": {
+            "noord": 18,
+            "zuid": 16
+        }
+    },
+    {
+        
+        "img":"img/19.png",
         "directions": {
             "noord": 19,
             "zuid": 17
@@ -161,97 +169,89 @@ let locatie =[
     },
     {
         
-        "img":"img/19.png",
-        "directions": {
-            "noord": 20,
-            "zuid": 18
-        }
-    },
-    {
-        
         "img":"img/20.png",
         "directions": {
-            "zuid": 19,
-            "noord": 21
+            "zuid": 18,
+            "noord": 20
         }
     },
     {
         
         "img":"img/21.png",
         "directions": {
-            "noord": 20,
-            "west": 22,
-            "zuid": 13
+            "noord": 19,
+            "west": 21,
+            "zuid": 12
         }
     },
     {
         
         "img":"img/22.png",
         "directions": {
-            "noord": 23,
-            "zuid": 21,
-            "oost": 28
+            "noord": 22,
+            "zuid": 20,
+            "oost": 27
         }
     },
     {
         
         "img":"img/23.png",
         "directions": {
-            "zuid": 22,
-            "noord": 24
+            "zuid": 21,
+            "noord": 23
         }
     },
     {
         
         "img":"img/24.png",
         "directions": {
-            "noord": 25,
-            "zuid": 23
+            "noord": 24,
+            "zuid": 22
         }
     },
     {
         
         "img":"img/25.png",
         "directions": {
-            "zuid": 24,
-            "oost": 26
+            "zuid": 23,
+            "oost": 25
         }
     },
     {
         
         "img":"img/26.png",
         "directions": {
-            "zuid": 25,
-            "west": 27
+            "zuid": 24,
+            "west": 26
     }
     },
     {
         
         "img":"img/27.png",
         "directions": {
-            "zuid": 26}
+            "zuid": 25}
     },
     {
         
         "img":"img/28.png",
         "directions": {
-            "noord": 29,
-            "zuid": 22
+            "noord": 28,
+            "zuid": 21
         }
     },
     {
         
         "img":"img/29.png",
         "directions": {
-            "noord": 30,
-            "zuid": 28
+            "noord": 29,
+            "zuid": 27
         }
     },
     {
         
         "img":"img/30.png",
         "directions": {
-            "zuid": 29
+            "zuid": 28
         }
     },
 ]
